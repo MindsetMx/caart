9f7c82953dc7dfe214aa6ad4241fcc376d45fd08
Fernando Velazquez <fernandovelaz96@gmail.com>
2023-12-23 16:38:26 -0600

    Add form group and import dependencies for general information component

--- src/app/register-car/complete-car-registration.service.ts
+++ src/app/register-car/complete-car-registration.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { GeneralInformationComponent } from './components/general-information/general-information.component';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
--- src/app/register-car/components/general-information/general-information.component.ts
+++ src/app/register-car/components/general-information/general-information.component.ts
@@ -1,14 +1,77 @@
+import { ChangeDetectionStrategy, Component, ViewChild, WritableSignal, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StripeCardComponent, StripeElementsDirective, injectStripe } from 'ngx-stripe';
+import { StripeCardElementOptions, StripeElementsOptions } from '@stripe/stripe-js';
+
+import { environments } from '@env/environments';
+import { InputDirective } from '@shared/directives/input.directive';
+import { InputErrorComponent } from '@shared/components/input-error/input-error.component';
+import { ValidatorsService } from '@shared/services/validators.service';
+import { PrimaryButtonDirective } from '@shared/directives/primary-button.directive';
 
 @Component({
   selector: 'register-car-general-information',
   standalone: true,
   imports: [
     CommonModule,
+    InputDirective,
+    InputErrorComponent,
+    PrimaryButtonDirective,
+    StripeCardComponent,
+    StripeElementsDirective,
   ],
   templateUrl: './general-information.component.html',
   styleUrl: './general-information.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GeneralInformationComponent { }
+export class GeneralInformationComponent {
+  @ViewChild(StripeCardComponent) card!: StripeCardComponent;
+  readonly #stripePublishableKey = environments.stripe.publishableKey;
+
+  cardOptions: StripeCardElementOptions = {
+    style: {
+      base: {
+        iconColor: '#666EE8',
+        color: '#31325F',
+        fontWeight: '300',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSize: '18px',
+        '::placeholder': {
+          color: '#CFD7E0'
+        }
+      }
+    }
+  };
+
+  elementsOptions: StripeElementsOptions = {
+    locale: 'es'
+  };
+
+  stripe = injectStripe(this.#stripePublishableKey);
+
+  #validatorsService = inject(ValidatorsService);
+  #fb = inject(FormBuilder);
+
+  generalInformationForm: FormGroup;
+
+  isButtonSubmitDisabled: WritableSignal<boolean> = signal(false);
+
+  constructor() {
+    this.generalInformationForm = this.#fb.group({
+      rfc: ['', [Validators.required, Validators.minLength(12), Validators.maxLength(13)]],
+      paquete: ['', [Validators.required]],
+      stripeToken: ['', [Validators.required]],
+    });
+  }
+
+  hasError(field: string): boolean {
+    return this.#validatorsService.hasError(this.generalInformationForm, field);
+  }
+
+  getError(field: string): string | undefined {
+    if (!this.generalInformationForm) return undefined;
+
+    return this.#validatorsService.getError(this.generalInformationForm, field);
+  }
+}
