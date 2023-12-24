bb484a29b976dd71629f44b99e5616c6711b0f5b
Fernando Velazquez <fernandovelaz96@gmail.com>
2023-12-24 13:15:26 -0600

    Crear formulario mecánica

--- src/app/register-car/complete-car-registration.service.ts
+++ src/app/register-car/complete-car-registration.service.ts
@@ -3,14 +3,15 @@ import { GeneralInformationComponent } from './components/general-information/ge
 
 import { GeneralDetailsAndExteriorOfTheCarComponent } from './components/general-details-and-exterior-of-the-car/general-details-and-exterior-of-the-car.component';
 import { InteriorOfTheCarComponent } from './components/interior-of-the-car/interior-of-the-car.component';
+import { MechanicsComponent } from './components/mechanics/mechanics.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompleteCarRegistrationService {
-  steps = [GeneralInformationComponent, GeneralDetailsAndExteriorOfTheCarComponent, InteriorOfTheCarComponent];
+  steps = [GeneralInformationComponent, GeneralDetailsAndExteriorOfTheCarComponent, InteriorOfTheCarComponent, MechanicsComponent];
 
-  indexCurrentStep: WritableSignal<number> = signal(1);
+  indexCurrentStep: WritableSignal<number> = signal(3);
 
   changeStep(step: number) {
     if (step < 0 || step >= this.steps.length) return;
---
+++ src/app/register-car/components/mechanics/mechanics.component.ts
@@ -0,0 +1,95 @@
+import { ChangeDetectionStrategy, Component, WritableSignal, inject, signal } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { AutoResizeTextareaDirective } from '@shared/directives/auto-resize-textarea.directive';
+import { InputDirective } from '@shared/directives/input.directive';
+import { InputErrorComponent } from '@shared/components/input-error/input-error.component';
+import { PrimaryButtonDirective } from '@shared/directives/primary-button.directive';
+import { ValidatorsService } from '@shared/services/validators.service';
+
+@Component({
+  selector: 'app-mechanics',
+  standalone: true,
+  imports: [
+    AutoResizeTextareaDirective,
+    InputDirective,
+    InputErrorComponent,
+    PrimaryButtonDirective,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './mechanics.component.html',
+  styleUrl: './mechanics.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MechanicsComponent {
+  #validatorsService = inject(ValidatorsService);
+  #fb = inject(FormBuilder);
+
+  mechanicsForm: FormGroup;
+
+  isButtonSubmitDisabled: WritableSignal<boolean> = signal(false);
+  previewImagesCarDetails: WritableSignal<string[]> = signal(['', '']);
+  previewImagesCarExterior: WritableSignal<string[]> = signal(['', '']);
+
+  constructor() {
+    this.mechanicsForm = this.#fb.group({
+      hasOriginalRims: ['', [Validators.required]],
+      tireBrand: ['', [Validators.required]],
+      tireCondition: ['', [Validators.required]],
+      hasExtraTiresOrRims: ['', [Validators.required]],
+      hasSpareTire: ['', [Validators.required]],
+      hasOriginalTransmissionAndMotor: ['', [Validators.required]],
+      hasGeneralImprovementOrModification: ['', [Validators.required]],
+      servicesPerformedWithDates: ['', [Validators.required]],
+      hasMechanicalProblemOrDetail: ['', [Validators.required]],
+      hasSensorLitOnTheDashboard: ['', [Validators.required]],
+      hasFactoryEquipment: ['', [Validators.required]],
+      hasExtraEquipment: ['', [Validators.required]],
+      commentsOnDetails: ['', [Validators.required]],
+      mechanicsImagesOrVideos: this.#fb.array([
+        [''],
+      ], [Validators.required]),
+    });
+  }
+
+  get mechanicsImagesOrVideosFormArray(): FormArray {
+    return this.mechanicsForm.get('mechanicsImagesOrVideos') as FormArray;
+  }
+
+  submitExteriorOfTheCarForm(): void {
+  }
+
+  selectFile(event: Event, indice: number): void {
+    const inputElement = event.target as HTMLInputElement;
+
+    if (!inputElement.files?.length) return;
+
+    const file = inputElement.files[0];
+    this.mechanicsImagesOrVideosFormArray.at(indice).setValue(file);
+
+    this.showPreview(file, indice);
+  }
+
+  showPreview(archivo: File, indice: number): void {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.previewImagesCarDetails.set(
+        this.previewImagesCarDetails().map((image, index) => {
+          if (index === indice) return reader.result as string;
+          return image;
+        })
+      );
+    }
+    reader.readAsDataURL(archivo);
+  }
+
+  hasError(field: string): boolean {
+    return this.#validatorsService.hasError(this.mechanicsForm, field);
+  }
+
+  getError(field: string): string | undefined {
+    if (!this.mechanicsForm) return undefined;
+
+    return this.#validatorsService.getError(this.mechanicsForm, field);
+  }
+}
