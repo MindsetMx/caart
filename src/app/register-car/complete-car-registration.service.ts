377be15cd0d31683aad6f1314a065d119c5a2eba
Fernando Velazquez <fernandovelaz96@gmail.com>
2023-12-24 11:48:04 -0600

    Crear formulario interior del auto

--- src/app/register-car/complete-car-registration.service.ts
+++ src/app/register-car/complete-car-registration.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, WritableSignal, signal } from '@angular/core';
 import { GeneralInformationComponent } from './components/general-information/general-information.component';
-import { FormGroup } from '@angular/forms';
+
 import { GeneralDetailsAndExteriorOfTheCarComponent } from './components/general-details-and-exterior-of-the-car/general-details-and-exterior-of-the-car.component';
+import { InteriorOfTheCarComponent } from './components/interior-of-the-car/interior-of-the-car.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompleteCarRegistrationService {
-  steps = [GeneralInformationComponent, GeneralDetailsAndExteriorOfTheCarComponent];
+  steps = [GeneralInformationComponent, GeneralDetailsAndExteriorOfTheCarComponent, InteriorOfTheCarComponent];
 
   indexCurrentStep: WritableSignal<number> = signal(1);
 
---
+++ src/app/register-car/components/interior-of-the-car/interior-of-the-car.component.ts
@@ -0,0 +1,87 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, WritableSignal, inject, signal } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputErrorComponent } from '@shared/components/input-error/input-error.component';
+import { AutoResizeTextareaDirective } from '@shared/directives/auto-resize-textarea.directive';
+import { InputDirective } from '@shared/directives/input.directive';
+import { PrimaryButtonDirective } from '@shared/directives/primary-button.directive';
+import { ValidatorsService } from '@shared/services/validators.service';
+
+@Component({
+  selector: 'app-interior-of-the-car',
+  standalone: true,
+  imports: [
+    AutoResizeTextareaDirective,
+    InputDirective,
+    InputErrorComponent,
+    PrimaryButtonDirective,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './interior-of-the-car.component.html',
+  styleUrl: './interior-of-the-car.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InteriorOfTheCarComponent {
+  #validatorsService = inject(ValidatorsService);
+  #fb = inject(FormBuilder);
+
+  interiorOfTheCarForm: FormGroup;
+
+  isButtonSubmitDisabled: WritableSignal<boolean> = signal(false);
+  previewImagesCarInterior: WritableSignal<string[]> = signal(['', '']);
+
+  constructor() {
+    this.interiorOfTheCarForm = this.#fb.group({
+      color: ['', [Validators.required]],
+      material: ['', [Validators.required]],
+      interiorCondition: ['', [Validators.required]],
+      hasInteriorModification: ['', [Validators.required]],
+      accessoriesAndElectronicsWork: ['', [Validators.required]],
+      detailsComments: ['', [Validators.required]],
+      interiorImagesOrVideos: this.#fb.array([
+        ['', [Validators.required]],
+      ], [Validators.required]),
+    });
+  }
+
+  get photosOrVideosInteriorOfTheCarFormArray(): FormArray {
+    return this.interiorOfTheCarForm.get('interiorImagesOrVideos') as FormArray;
+  }
+
+  submitExteriorOfTheCarForm(): void {
+  }
+
+  selectFile(event: Event, indice: number): void {
+    const inputElement = event.target as HTMLInputElement;
+
+    if (!inputElement.files?.length) return;
+
+    const file = inputElement.files[0];
+    this.photosOrVideosInteriorOfTheCarFormArray.at(indice).setValue(file);
+
+    this.showPreview(file, indice);
+  }
+
+  showPreview(archivo: File, indice: number): void {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.previewImagesCarInterior.set(
+        this.previewImagesCarInterior().map((image, index) => {
+          if (index === indice) return reader.result as string;
+          return image;
+        })
+      );
+    }
+    reader.readAsDataURL(archivo);
+  }
+
+  hasError(field: string): boolean {
+    return this.#validatorsService.hasError(this.interiorOfTheCarForm, field);
+  }
+
+  getError(field: string): string | undefined {
+    if (!this.interiorOfTheCarForm) return undefined;
+
+    return this.#validatorsService.getError(this.interiorOfTheCarForm, field);
+  }
+}
