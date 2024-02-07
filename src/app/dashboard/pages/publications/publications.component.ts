8ef33a0ab66be1e79ccff7b828bb20e2d3a6c593
Fernando Velazquez <fernandovelaz96@gmail.com>
2024-02-07 11:00:08 -0600

    Crear tabla publicaciones en dashboard

--- src/app/dashboard/pages/publications/publications.component.ts
+++ src/app/dashboard/pages/publications/publications.component.ts
@@ -1,6 +1,12 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, effect, inject, signal } from '@angular/core';
+
+import { AuctionCarPublicationsData } from '@app/auctions/interfaces/auction-car-publishes';
+import { AuctionService } from '@app/auctions/services/auction.service';
 import { SidebarComponent } from '@app/dashboard/layout/sidebar/sidebar.component';
+import { AuthStatus } from '@auth/enums';
+import { AuthService } from '@auth/services/auth.service';
+import { GeneralInfoService } from '@auth/services/general-info.service';
+import { EMPTY, Observable, catchError, map } from 'rxjs';
 
 @Component({
   selector: 'app-publications',
@@ -12,4 +18,24 @@ import { SidebarComponent } from '@app/dashboard/layout/sidebar/sidebar.componen
   styleUrl: './publications.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PublicationsComponent { }
+export class PublicationsComponent implements OnInit {
+  #auctionService = inject(AuctionService);
+  #authService = inject(AuthService);
+  #generalInfoService = inject(GeneralInfoService);
+
+  auctionCarPublishes = signal<AuctionCarPublicationsData[]>([]);
+
+  get authStatus(): AuthStatus {
+    return this.#authService.authStatus();
+  }
+
+  ngOnInit(): void {
+    this.getAuctionCarPublishes();
+  }
+
+  getAuctionCarPublishes(): void {
+    this.#auctionService.auctionCarPublications$().subscribe((auctionCarPublishes) => {
+      this.auctionCarPublishes.set(auctionCarPublishes.data);
+    });
+  }
+}
