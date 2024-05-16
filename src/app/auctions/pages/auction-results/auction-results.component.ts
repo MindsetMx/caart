a965d17b7365ac25efffe1a4a8b8abb6334fef5c
Fernando Velazquez <fernandovelaz96@gmail.com>
2024-05-16 10:28:21 -0600

    Hacer que los query params de resultados no se restablezcan al recargar la pagina

--- src/app/auctions/pages/auction-results/auction-results.component.ts
+++ src/app/auctions/pages/auction-results/auction-results.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuctionResultsVehicleFilterResultsComponent } from '@app/auction-results/pages/auction-results-vehicle-filter-results/auction-results-vehicle-filter-results.component';
 import { VehicleFilterResultsComponent } from '@auctions/components/vehicle-filter-results/vehicle-filter-results.component';
 import { TabsWithIconsComponent } from '@shared/components/tabs-with-icons/tabs-with-icons.component';
@@ -18,41 +18,49 @@ import { TabWithIcon } from '@shared/interfaces/tabWithIcon';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuctionResultsComponent {
-  tabs: TabWithIcon[];
-  currentTab = signal<TabWithIcon>({} as TabWithIcon);
+  tabs?: TabWithIcon[];
+  currentTab = signal<number | undefined>(undefined);
 
   #router = inject(Router);
+  #activatedRoute = inject(ActivatedRoute);
 
   constructor() {
-    this.tabs =
-      [
-        {
-          id: 1,
-          name: 'Todo',
-          img: 'assets/img/icons/apps-outline.svg',
-          current: false
-        },
-        {
-          id: 2,
-          name: 'Automóviles',
-          img: 'assets/img/registrar auto/car-sport-outline.svg',
-          current: true
-        },
-        {
-          id: 3,
-          name: 'Arte',
-          img: 'assets/img/registrar auto/milo-venus.svg',
-          current: false
-        },
-      ];
+    this.#activatedRoute.queryParams.subscribe(params => {
+      let tabId = +params['tab'];
 
-    this.currentTab.set(this.tabs[1]);
+      if (isNaN(tabId)) {
+        tabId = 1;
+      }
 
-    this.navigateWithQueryParams(this.currentTab().id);
+      this.currentTab.set(tabId);
+
+
+      this.tabs =
+        [
+          {
+            id: 1,
+            name: 'Todo',
+            img: 'assets/img/icons/apps-outline.svg',
+            current: tabId === 1
+          },
+          {
+            id: 2,
+            name: 'Automóviles',
+            img: 'assets/img/registrar auto/car-sport-outline.svg',
+            current: tabId === 2
+          },
+          {
+            id: 3,
+            name: 'Arte',
+            img: 'assets/img/registrar auto/milo-venus.svg',
+            current: tabId === 3
+          },
+        ];
+    });
   }
 
   onTabSelected(tab: TabWithIcon): void {
-    this.currentTab.set(tab);
+    this.currentTab.set(tab.id);
 
     this.navigateWithQueryParams(tab.id);
   }
